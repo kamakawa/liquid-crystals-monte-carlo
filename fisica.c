227112c200addc8af99f92b522f90d4faac54e0c
Eric Kamakawa <erickamakawa@alunos.utfpr.edu.br>
2025-04-04 18:43:01 -0300

    Add files via upload

--- fisica.c
+++ fisica.c
@@ -3,15 +3,16 @@
 #include <math.h>
 #include <time.h>
 
-#define L 20       // Tamanho da grade, pode ser alterada
-#define J 1        // Constante de interação (pode ser 1 para simplificar)
-#define H 0.5      // Campo magnético externo, pode ser alterada
+
+#define L 10       // Tamanho da grade (pode ser alterado)
+#define J 1        // Constante de interação (normalmente 1)
+#define H 0.5      // Campo magnético externo (pode ser alterado)
 
 int spins[L][L];
 
-// Função para inicializar os spins aleatoriamente
+// Função para inicializar os spins aleatoriamente (+1 ou -1)
 void inicializarSpins() {
-    srand(time(NULL));  // Semente para números aleatórios
+    srand(time(NULL));  // Gerador de números aleatórios
     for (int i = 0; i < L; i++) {
         for (int j = 0; j < L; j++) {
             spins[i][j] = (rand() % 2) ? 1 : -1;
@@ -19,86 +20,87 @@ void inicializarSpins() {
     }
 }
 
-// Função para imprimir a rede de spins com símbolos O e X
+// Função para imprimir a rede de spins
 void imprimirSpins() {
     for (int i = 0; i < L; i++) {
         for (int j = 0; j < L; j++) {
-            if (spins[i][j] == 1) {
-                printf("O ");  // Spin para cima (O), +1
-            } else {
-                printf("X ");  // Spin para baixo (X), -1
-            }
+            printf("%c ", (spins[i][j] == 1) ? 'O' : 'X');
         }
         printf("\n");
     }
 }
 
-// Função para calcular a energia local de um spin (com campo magnético)
-int calcularEnergia(int i, int j) {
-    int cima = spins[(i - 1 + L) % L][j];
-    int baixo = spins[(i + 1) % L][j];
-    int esquerda = spins[i][(j - 1 + L) % L];
-    int direita = spins[i][(j + 1) % L];
-    int somaVizinhos = cima + baixo + esquerda + direita;
-    int energia = -J * spins[i][j] * somaVizinhos - H * spins[i][j];
-    return energia;
-}
-
-// Função para calcular a energia total da rede
-float calcularEnergiaTotal() {
-    float energiaTotal = 0.0;
-    for (int i = 0; i < L; i++) {
-        for (int j = 0; j < L; j++) {
-            energiaTotal += calcularEnergia(i, j);
-        }
-    }
-    return energiaTotal / 2.0;  // Dividir por 2 para evitar contar duas vezes
-}
-
-// Função para calcular a magnetização total
-float calcularMagnetizacao() {
+// Função para calcular a magnetização média
+double calcularMagnetizacao() {
     int soma = 0;
+    int N = L * L;  // Número total de spins na grade
+    
     for (int i = 0; i < L; i++) {
         for (int j = 0; j < L; j++) {
             soma += spins[i][j];
         }
     }
-    return (float)soma / (L * L);  // Magnetização média
+    
+    return (double)soma / N;  // Converte para double para evitar truncamento
 }
 
-// Função para calcular a variação de energia ao inverter o spin
+// Função para calcular a energia local de um spin
+int calcularEnergia(int i, int j) {
+    int cima = spins[(i - 1 + L) % L][j];
+    int baixo = spins[(i + 1) % L][j];
+    int esquerda = spins[i][(j - 1 + L) % L];
+    int direita = spins[i][(j + 1) % L];
+
+    int somaVizinhos = cima + baixo + esquerda + direita;
+    return -J * spins[i][j] * somaVizinhos - H * spins[i][j];
+}
+
+// Função para calcular a energia total da rede
+double calcularEnergiaTotal() {
+    double energiaTotal = 0.0;
+    
+    for (int i = 0; i < L; i++) {
+        for (int j = 0; j < L; j++) {
+            energiaTotal += calcularEnergia(i, j);
+        }
+    }
+
+    return energiaTotal / 4.0;  // Dividimos por 4 para evitar contagem excessiva
+}
+
+// Função para calcular a variação de energia ao inverter um spin
 int deltaEnergia(int i, int j) {
     int energiaAtual = calcularEnergia(i, j);
-    spins[i][j] *= -1;
+    spins[i][j] *= -1;  // Inverte temporariamente o spin
     int energiaNova = calcularEnergia(i, j);
-    spins[i][j] *= -1;
+    spins[i][j] *= -1;  // Reverte a inversão
     return energiaNova - energiaAtual;
 }
 
 // Função para realizar uma etapa de Monte Carlo
-void monteCarloStep(float temperatura) {
+void monteCarloStep(double temperatura) {
     for (int i = 0; i < L; i++) {
         for (int j = 0; j < L; j++) {
             int dE = deltaEnergia(i, j);
-            float prob = exp(-dE / temperatura);
-            if (dE <= 0 || ((float)rand() / RAND_MAX) < prob) {
-                spins[i][j] *= -1;
+            double prob = exp(-dE / temperatura);
+            if (dE <= 0 || ((double)rand() / RAND_MAX) < prob) {
+                spins[i][j] *= -1;  // Aceita a mudança de estado
             }
         }
     }
 }
 
 // Função principal
-int main() {
-    int iteracoes = 10000;
-    float temperatura = 2.5;
+int main(int argc, char *argv[]) {
+    int iteracoes = atof(argv[1]);      //Número de pontos sorteados
+    double temperatura = atof(argv[2]);
 
-    inicializarSpins();
+    inicializarSpins();     //cria a primeira grade de spins aleatórios
     printf("Rede de spins inicial:\n");
-    imprimirSpins();
+    imprimirSpins();    //função que mostra a grade gerada
 
-    float energiaMedia = 0.0;
-    float magnetizacaoMedia = 0.0;
+    double energiaMedia = 0.0;
+    double magnetizacaoMedia = 0.0;
 
     for (int t = 0; t < iteracoes; t++) {
         monteCarloStep(temperatura);
@@ -108,10 +110,12 @@ int main() {
 
     energiaMedia /= iteracoes;
     magnetizacaoMedia /= iteracoes;
+    
     printf("\nRede de spins apos a simulacao:\n");
     imprimirSpins();
     printf("\nEnergia media: %.4f\n", energiaMedia);
-    printf("Magnetizacao media: %.4f\n", magnetizacaoMedia);
+    printf("Magnetização media: %.4f\n", magnetizacaoMedia);
     
     return 0;
 }
+
